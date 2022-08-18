e5895eb499e4f4e7d24875c690507ba9e014ba23
ATWA66 <98480275+ATWA66@users.noreply.github.com>
2022-08-19 01:08:21 +0300

    Add files via upload

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,12 @@
+﻿//6
+int[] myArr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+Console.WriteLine("before");
+for (int i = 0; i < myArr.Length; i++)
+{
+    Console.WriteLine(myArr[i]);
+}
+Console.WriteLine("after");
+for (int i = myArr.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(myArr[i]);
+}
